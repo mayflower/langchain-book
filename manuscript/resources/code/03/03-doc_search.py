b41bfb89e3c498858a0068df3f3b09883bf1ca01
Andreas Haberberger <andreas@haberberger.com>
2023-06-28 13:53:21 +0200

    Redaktion Kap 3 - Mitte

--- manuscript/resources/code/03/03-doc_search.py
+++ manuscript/resources/code/03/03-doc_search.py
@@ -1,29 +1,25 @@
-1 from langchain.text_splitter import CharacterTextSplitter
-2 from langchain.vectorstores import Chroma
-3 from langchain.embeddings import OpenAIEmbeddings
-4 from langchain.document_loaders import DirectoryLoader
-5 from langchain import OpenAI, VectorDBQA
-6
-7 embeddings = OpenAIEmbeddings() 8
-9 loader=DirectoryLoader('../data/', glob="**/*.txt")
-10 documents = loader.load()
-11 text_splitter = CharacterTextSplitter(chunk_size=2500, ch\
-12 unk_overlap=0)
-13
-14 texts = text_splitter.split_documents(documents)
-15
-16 docsearch = Chroma.from_documents(texts, embeddings)
-17
-18 qa = VectorDBQA.from_chain_type(llm=OpenAI(),
-19                                 chain_type="stuff",
-20                                 vectorstore=docsearch)
-21
-22 def query(q):
-23     print(f"Query: {q}")
-24     print(f"Answer: {qa.run(q)}")
-25
-26 query("What kinds of equipment are in a chemistry laborat\
-27 ory?")
-28 query("What is Austrian School of Economics?")
-29 query("Why do people engage in sports?")
-30 query("What is the effect of bodychemistry on exercise?")
+from langchain.text_splitter import CharacterTextSplitter
+from langchain.vectorstores import Chroma
+from langchain.embeddings import OpenAIEmbeddings
+from langchain.document_loaders import DirectoryLoader
+from langchain import OpenAI, VectorDBQA
+
+embeddings = OpenAIEmbeddings() 
+loader=DirectoryLoader('../data/', glob="**/*.txt")
+documents = loader.load()
+text_splitter = CharacterTextSplitter(chunk_size=2500, chunk_overlap=0)
+
+texts = text_splitter.split_documents(documents)
+
+docsearch = Chroma.from_documents(texts, embeddings)
+
+qa = VectorDBQA.from_chain_type(llm=OpenAI(), chain_type="stuff", vectorstore=docsearch)
+
+def query(q):
+    print(f"Query: {q}")
+    print(f"Answer: {qa.run(q)}")
+
+query("What kinds of equipment are in a chemistry laboratory?")
+query("What is Austrian School of Economics?")
+query("Why do people engage in sports?")
+query("What is the effect of bodychemistry on exercise?")
