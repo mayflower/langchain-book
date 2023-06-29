199346501596c9e66b0fa4ec12ada23d8f737d94
Maris Klein <maris.klein@mayflower.de>
2023-06-29 10:51:33 +0200

    Update 2 files
    - /manuscript/resources/code/05/05-example client calling.py
    - /manuscript/05-Using_Googles_Knowledge_Graph_APIs_With_LangChain

---
+++ manuscript/resources/code/05/05-example client calling.py
@@ -0,0 +1,20 @@
+"""Example of Python client calling the
+   Knowledge Graph Search API."""
+from llama_index import GPTListIndex, Document
+import Google_KG_helper
+def kg_search(entity_name, *questions):
+     ret = ""
+     context_text =
+       Google_KG_helper.get_context_text(entity_name)
+     print(f"Context text: {context_text}")
+     doc = Document(context_text)
+     index = GPTListIndex([doc])
+     for question in questions:
+         response = index.query(question)
+         ret +=
+           f"QUESTION: {question}\nRESPONSE: {response}\n"
+     return ret
+ if __name__ == "__main__":
+     s = kg_search("Bill Clinton",
+                   "When was Bill president?")
+     print(s)
