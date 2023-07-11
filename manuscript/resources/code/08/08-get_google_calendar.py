4de5687c8a6301223b01e0d5addca1dfb72778e9
Andreas Haberberger <andreas@haberberger.com>
2023-07-11 13:16:55 +0200

    Chapter 08 - final content

--- manuscript/resources/code/08/08-get_google_calendar.py
+++ manuscript/resources/code/08/08-get_google_calendar.py
@@ -3,11 +3,9 @@ fromlangchain.agentsimportinitialize_agent
 fromlangchain.agents.agent_toolkitsimportZapierToolkit
 fromlangchain.utilities.zapierimportZapierNLAWrapper
 
-llm=OpenAI(temperature=0)
-zapier=ZapierNLAWrapper()
-toolkit=ZapierToolkit.from_zapier_nla_wrapper(zapier)
-agent=initialize_agent(toolkit.get_tools(),llm,
-agent="zero-shot-react-descripti\
-on",verbose=True)
+llm = OpenAI(temperature=0)
+zapier = ZapierNLAWrapper()
+toolkit = ZapierToolkit.from_zapier_nla_wrapper(zapier)
+agent = initialize_agent(toolkit.get_tools(), llm, agent = "zero-shot-react-description", verbose=True)
 
-agent.run("GetmyGoogleCalendarentriesfortomorrow")
+agent.run("Get my Google Calendar entries for tomorrow")
