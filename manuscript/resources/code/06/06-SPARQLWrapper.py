a19f28d89c4589c7a99e1d585b68df47151ef021
Andreas Haberberger <andreas@haberberger.com>
2023-07-11 09:13:31 +0200

    Chapter 06 - final content

--- manuscript/resources/code/06/06-SPARQLWrapper.py
+++ manuscript/resources/code/06/06-SPARQLWrapper.py
@@ -7,34 +7,32 @@ sparql.setQuery("""
    PREFIX dbpedia: <http://dbpedia.org/resource>
    PREFIX dbpprop: <http://dbpedia.org/property>
 
- CONSTRUCT {
-    ?city dbpedia-owl:country ?country .
-    ?city rdfs:label ?citylabel .
-    ?country rdfs:label ?countrylabel .
-    <http://dbpedia.org/ontology/country> rdfs:label "country"@en. 
-    }
-    WHERE {
-            ?city rdf:type dbpedia-owl:City .
-            ?city rdfs:label ?citylabel .
-            ?city dbpedia-owl:country ?country .
-            ?country rdfs:label ?countrylabel .
-            FILTER (lang(?citylabel) = 'en')
-            FILTER (lang(?countrylabel) = 'en')
-         }
-        LIMIT 50
- """)
- sparql.setReturnFormat("rdf")
- results = sparql.query().convert()
+   CONSTRUCT {
+      ?city dbpedia-owl:country ?country .
+      ?city rdfs:label ?citylabel .
+      ?country rdfs:label ?countrylabel .
+      <http://dbpedia.org/ontology/country> rdfs:label "country"@en. 
+      }
+   WHERE {
+         ?city rdf:type dbpedia-owl:City .
+         ?city rdfs:label ?citylabel .
+         ?city dbpedia-owl:country ?country .
+         ?country rdfs:label ?countrylabel .
+         FILTER (lang(?citylabel) = 'en')
+         FILTER (lang(?countrylabel) = 'en')
+      }
+   LIMIT 50
+""")
+sparql.setReturnFormat("rdf")
+results = sparql.query().convert()
 
- g = Graph()
- g.parse(data=results.serialize(format="xml"), format="xml\
- ")
+g = Graph()
+g.parse(data=results.serialize(format="xml"), format="xml")
 
- print("\nresults:\n")
- results = g.serialize(format="nt").encode("utf-8").decode\
- ('utf-8')
- print(results)
+print("\nresults:\n")
+results = g.serialize(format="nt").encode("utf-8").decode('utf-8')
+print(results)
 
- text_file = open("sample.nt", "w")
- text_file.write(results)
- text_file.close()
+text_file = open("sample.nt", "w")
+text_file.write(results)
+text_file.close()
