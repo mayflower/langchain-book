fa6fcaeb4726379c20ccef5b7a79ebc5cf9d9eee
Andreas Haberberger <andreas@haberberger.com>
2023-07-27 19:02:50 +0200

    Chapter 11 - final content

--- manuscript/resources/code/11/11-recipe_generator.py
+++ manuscript/resources/code/11/11-recipe_generator.py
@@ -8,22 +8,20 @@ embeddings = OpenAIEmbeddings()
 
 loader = DirectoryLoader('./text_data/', glob="**/*.txt")
 documents = loader.load()
-text_splitter = CharacterTextSplitter(chunk_size=2500,
-chunk_overlap=0)
+text_splitter = CharacterTextSplitter(chunk_size=2500, chunk_overlap=0)
 
 texts = text_splitter.split_documents(documents)
 
 docsearch = Chroma.from_documents(texts, embeddings)
 
 qa = VectorDBQA.from_chain_type(llm=OpenAI(temperature=0,
-model_name=
-"text-davinci-002"),
-chain_type="stuff",
-vectorstore=docsearch)
+    model_name="text-davinci-002"),
+    chain_type="stuff",
+    vectorstore=docsearch)
 
 def query(q):
-print(f"\n\nRecipe creation request: {q}\n")
-print(f"{qa.run(q)}\n\n")
+    print(f"\n\nRecipe creation request: {q}\n")
+    print(f"{qa.run(q)}\n\n")
 
 query("Create a new recipe using Broccoli and Chicken")
 query("Create a recipe using Beans, Rice, and Chicken")
