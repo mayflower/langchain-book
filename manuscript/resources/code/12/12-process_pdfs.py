d39b1f11ca3f9a25e8212cda08445f239abf6eae
Andreas Haberberger <andreas@haberberger.com>
2023-08-07 09:30:00 +0200

    Chapter 12 - final

---
+++ manuscript/resources/code/12/12-process_pdfs.py
@@ -0,0 +1,13 @@
+# reference: https://github.com/embedchain/embedchain
+
+from embedchain import App
+import os
+
+test_chat = App()
+
+my_books_dir = "/Users/mark/data/"
+
+for filename in os.listdir(my_books_dir):
+    if filename.endswith('.pdf'):
+        print("processing filename:", filename)
+        test_chat.add("pdf_file", os.path.join(my_books_dir, filename))
